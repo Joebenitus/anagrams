34b742fb570e4600dad25c27f96d426bb9872c97
Joseph Karnafel <josephkarnafel@gmail.com>
2020-09-25 15:10:54 -0700

    update palindrome? method to return false if the input is not a palindrome instead of nil

--- lib/anagrams.rb
+++ lib/anagrams.rb
@@ -21,7 +21,7 @@ class Anagram
   end
 
   def word?
-    if @word1.match(/[aeiouy]/i) & @word2.match(/[aeiouy]/i)
+    if (@word1.match(/[aeiouy]/i)) && (@word2.match(/[aeiouy]/i))
       true
     else
       false
@@ -32,7 +32,7 @@ class Anagram
     words_combined = (@word1_parsed + @word2_parsed).split('')
     (words_combined.length/2).times() do |letter|
       if words_combined[letter] != words_combined[(letter+1)*(-1)]
-        return
+        false
       end
     end
     "#{@word1} #{@word2} is a palindrome!"
