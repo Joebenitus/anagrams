ebb5c1b5b778863e4a89aeb9f9632b632fc96b21
Joseph Karnafel <josephkarnafel@gmail.com>
2020-09-30 13:36:43 -0700

    move app.rb file to project root directory

---
+++ app.rb
@@ -0,0 +1,17 @@
+require('./lib/anagrams')
+
+loop do
+  puts "\nEnter two words or phrases to find out if they are anagrams/antigrams"
+  puts "Word 1:"
+  word1 = gets.chomp
+  puts "Word 2:"
+  word2 = gets.chomp
+  anagram = Anagram.new(word1, word2)
+  puts anagram.palindrome?()
+  puts anagram.anagram_antigram()
+  puts "Would you like to enter more words? (yes/no)"
+  play_again = gets.chomp
+  if play_again === 'no'
+    break
+  end
+end
